c5d2190a3f7a7627c43db78b487eb943589915c1
zymazz319-arch <zymazz319@gmail.com>
2026-03-28 15:20:16 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'taskflow-v1';
+const assets = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
